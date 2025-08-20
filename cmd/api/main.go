aa76e40e22afae39aff7ea748cd7635ebc44ade0
Lucas Feijó <lucasfjportela@gmail.com>
2025-08-19 23:28:27 -0300

    feat: MongoDB driver (#1)
    * feat: Add `mongo-driver`
    
    * feat: Mongo connect

---
+++ cmd/api/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Connection error:", err)
+	}
+}
